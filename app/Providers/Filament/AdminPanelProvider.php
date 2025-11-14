79023d7838bb5456a605ae522b18b01072f0b04d
progSarida <programmazione@sarida.it>
2025-11-14 15:21:37 +0100

    Correzione

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
     public function panel(Panel $panel): Panel
     {
         return $panel
-            // ->default()
+            ->default()
             ->id('admin')
             ->path('admin')
             ->login()
